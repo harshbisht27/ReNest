96b3f94319910b9afb243e97ef364e81e7a89530
Harsh Bisht <168906728+harshbisht27@users.noreply.github.com>
2024-10-19 00:59:06 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:9000/send-email', {
+      const response = await fetch('https://renest.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
